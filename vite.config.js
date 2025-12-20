f835c7f931b8894f2f2ce20cb77237ad7d945e19
Yuri Marroquin Mejia <84044360+Yuri-cmd@users.noreply.github.com>
2025-12-20 08:44:48 -0500

    feat: Simplify Vite configuration by removing environment variable loading

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig(({ mode }) => {
-  // Carga variables .env para el modo (dev / production)
-  const env = loadEnv(mode, process.cwd(), '');
-
-  // Usa VITE_BASE si está definido en .env, por ejemplo: VITE_BASE=/genack/public/
-  // Si no está, por defecto se usa '/' (raíz).
-  const base = env.VITE_BASE || '/';
-
-  return {
-    base,
+export default defineConfig({
     plugins: [
-      laravel({
-        input: ['resources/css/app.css', 'resources/js/app.js'],
-        refresh: true,
-      }),
-      tailwindcss(),
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+        tailwindcss(),
     ],
-  };
 });
