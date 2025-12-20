1e4ec0b0f67bcbf413fec43978c490f7841dc334
Yuri Marroquin Mejia <84044360+Yuri-cmd@users.noreply.github.com>
2025-12-20 09:48:16 -0500

    fix: Correct class name casing for SubFamilia model in SubFamiliaController

--- app/Http/Controllers/SubFamiliaController.php
+++ app/Http/Controllers/SubFamiliaController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Subfamilia;
+use App\Models\SubFamilia;
 use App\Models\Familia;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
