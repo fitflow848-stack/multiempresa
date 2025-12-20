1d2f5186ed4b2dfd23222d02d73ff52a857db8e4
Yuri Marroquin Mejia <84044360+Yuri-cmd@users.noreply.github.com>
2025-12-20 09:29:07 -0500

    feat: Dynamically configure Livewire asset URL and update root URL for production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Config;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +20,13 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if (app()->environment('production')) {
+            // Forzamos a que Laravel sepa que su raíz es la subcarpeta
+            URL::forceRootUrl(config('app.url'));
+            
+            // Configuramos Livewire dinámicamente desde el código
+            Config::set('livewire.asset_url', '/genack/public');
+            Config::set('livewire.update_uri', '/genack/public/livewire/update');
+        }
     }
 }
