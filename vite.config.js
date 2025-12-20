ef197fcd109dcd02c1ea9816f0bb6089183e6e69
Yuri Marroquin Mejia <84044360+Yuri-cmd@users.noreply.github.com>
2025-12-20 09:25:02 -0500

    feat: Add asset URL configuration and update Vite base path for asset management

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    // AÑADE ESTA LÍNEA (Asegúrate de que coincida con tu carpeta)
+    base: '/genack/public/build/', 
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
